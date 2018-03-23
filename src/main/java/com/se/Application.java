7902076b0b43e3d010a75143e42e17b697cc3c4e
wswty <2804151301@qq.com>
2018-03-23 14:00:55 +0800

    修改端口为8080及增加热部署工具

--- src/main/java/com/se/Application.java
+++ src/main/java/com/se/Application.java
@@ -3,11 +3,7 @@ package com.se;
 import com.se.mapper.ChannelRepository;
 import com.se.mapper.HistoryRepository;
 import com.se.mapper.UserRepository;
-import com.se.model.Channel;
-import com.se.model.History;
-import com.se.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
@@ -34,6 +30,6 @@ public class Application implements EmbeddedServletContainerCustomizer {
 
     @Override
     public void customize(ConfigurableEmbeddedServletContainer container) {
-        container.setPort(8088);
+        container.setPort(8080);
     }
 }
